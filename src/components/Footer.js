b0601f9243719b5691afa8fe35cdd590339bfff1
kevans107 <kellyevans107.gmail.com>
2021-10-27 17:32:46 -0400

    working on UI

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component{
+  render(){
+    return(
+      <>
+        <h1>This is the Footer</h1>
+      </>
+    )
+  }
+}
+
+export default Footer
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NotFound extends Component{
+  render(){
+    return(
+      <>
+        <h1>This is NotFound</h1>
+      </>
+    )
+  }
+}
+
+export default NotFound
