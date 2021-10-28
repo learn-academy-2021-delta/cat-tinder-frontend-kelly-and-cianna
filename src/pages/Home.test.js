13d6d754865884070d61d01c77a67872ede74220
kevans107 <kellyevans107.gmail.com>
2021-10-28 19:48:04 -0400

    test coverage complete

---
+++ src/pages/Home.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Home from './Home'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("When Home renders", () => {
+    it("displays a heading", () => {
+      const homeWrapper = shallow(<Home />)
+      const showHeading = homeWrapper.find("h1")
+      expect(showHeading.text()).toEqual("This is Home")
+    })
+  })
+
+  
